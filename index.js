ec93a989971ed03340e1a5d19e696c266e54aeb6
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2022-12-28 20:05:00 +0700

    feat: server messages and routes

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const connect = require('./lib/database/connect.js');
 
 const server = tabri({
   port: config.port,
+  routes: require('./src/modules/routes.js'),
+  i18n: {
+    messages: require('./resources/messages'),
+    defaultLocale: 'en',
+  },
 });
 
 connect(config.db.url)

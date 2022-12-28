2761485fb8c48530cab1be1f1b4ef7a32cdde097
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2022-12-29 05:11:13 +0700

    feat: app lib

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-const config = require('./config');
-const tabri = require('@ibrahimalanshor/tabri');
-const connect = require('./lib/database/connect.js');
+const createApp = require('./lib/app.js');
 
-const server = tabri({
-  port: config.port,
-  routes: require('./src/modules/routes.js'),
-  i18n: {
-    messages: require('./resources/messages'),
-    defaultLocale: 'en',
-  },
-});
-
-connect(config.db.url)
-  .then(() => {
-    server.run();
-  })
-  .catch((err) => {
-    console.error(err);
-
-    process.exit(0);
-  });
+createApp();

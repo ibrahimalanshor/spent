6f8232729aa3fcc0cdd6c2154d9d5c0f3a9ea8a4
ibra <ibrahimalanshor6@gmail.com>
2022-12-29 13:48:03 +0800

    feat: reducing balance cannot lower than current balance amount

--- src/modules/balance-transaction/balance-transaction.controller.js
+++ src/modules/balance-transaction/balance-transaction.controller.js
@@ -2,6 +2,7 @@ const {
   exceptions: { NotFoundException, BadRequestException },
 } = require('@ibrahimalanshor/tabri');
 const Controller = require('../../../lib/controller/controller.js');
+const MathHelper = require('../../helpers/math.helper.js');
 const BalanceTransactionService = require('./balance-transaction.service.js');
 const BalanceService = require('../balance/balance.service.js');
 
@@ -16,6 +17,16 @@ exports.create = new Controller()
   .handle(async (ctx) => {
     try {
       const balance = await BalanceService.findOne(ctx.body.balanceId);
+
+      if (
+        MathHelper.isNegative(ctx.body.amount) &&
+        MathHelper.toAbsolute(ctx.body.amount) > balance.amount
+      ) {
+        throw new BadRequestException(
+          {},
+          'balance-transaction.validation.reducing-amount-to-large'
+        );
+      }
 
       return await BalanceTransactionService.create(ctx.body, {
         balance,

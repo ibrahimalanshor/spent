3c4c29f5ad1515ac484102881baaff701ef04378
ibra <ibrahimalanshor6@gmail.com>
2022-12-29 10:43:49 +0800

    feat: balance find and add balance

--- src/modules/balance/balance.service.js
+++ src/modules/balance/balance.service.js
@@ -1,4 +1,5 @@
 const BalanceQuery = require('./balance.query.js');
+const BalanceModel = require('./model/balance.model.js');
 
 exports.getAll = async function getAll(query) {
   const docs = await new BalanceQuery().paginate({
@@ -8,4 +9,18 @@ exports.getAll = async function getAll(query) {
   const count = await new BalanceQuery().count();
 
   return { count, docs };
+};
+
+exports.findOne = async function findOne(id) {
+  return await new BalanceQuery().findByIdOrFail(id);
+};
+
+exports.addBalance = async function addBalance(balance, amount, { session }) {
+  return await BalanceModel.updateOne(
+    balance,
+    {
+      $inc: { amount },
+    },
+    { session }
+  );
 };

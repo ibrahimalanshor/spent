cb45251fbc797a66504ae9348207a2483e5c7de2
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2022-12-31 20:32:51 +0700

    feat: upload public config

--- config/index.js
+++ config/index.js
@@ -1,9 +1,20 @@
+const path = require('path');
+
 if (process.env.NODE_ENV === 'development') require('dotenv/config');
+
+const appUrl = process.env.APP_URL || `http://localhost:4000`;
 
 module.exports = {
   app: {
     port: process.env.PORT || process.env.APP_PORT || 4000,
+    url: appUrl,
   },
+  upload_dir: path.resolve(
+    __dirname,
+    '../',
+    process.env.UPLOAD_DIR || 'public'
+  ),
+  public_url: `${appUrl}/public`,
   db: {
     url: process.env.DB_URL,
   },

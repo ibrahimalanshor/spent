bd478bc173282b36bbbcebd3a542c0fba7b77964
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2023-01-01 10:53:16 +0700

    feat: balance timestamps

--- src/modules/balance/model/balance.schema.js
+++ src/modules/balance/model/balance.schema.js
@@ -1,10 +1,15 @@
 const { Schema } = require('mongoose');
 
-const BalanceSchema = new Schema({
-  amount: {
-    type: Number,
-    default: 0,
+const BalanceSchema = new Schema(
+  {
+    amount: {
+      type: Number,
+      default: 0,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = BalanceSchema;
